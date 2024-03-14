4aa24114f9708f28e03181c3f50508f4b8e92cff
Othallia <vianiputriocta6@gmail.com>
2024-03-14 17:41:00 +0700

    menambahkan prosedur

--- 032_Insertionsort/dllmain.cpp
+++ 032_Insertionsort/dllmain.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void insertionsort() {
+void insertionsort() {  //prosedur
 	int i, n, temp, j;
 	int arr[9];
+
+	for (i = 1; i <= n - 1; i++) { //step 1
+
+		temp = arr[i]; //step 2
+
+		j = i - 1; //step 3
+
+		while (j >= 0 && arr[j] > temp) //step 4
+		{
+			arr[j + 1] = arr[j]; //step 4a
+			j--; //step 4b
+		}
+		arr[j + 1] = temp; //step 5
+	}
 }
 
 int main()
